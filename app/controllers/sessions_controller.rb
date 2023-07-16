1f5e2db5e9983933b13f865e060e3cb2604b5a86
s3155 <yutaka.7sn@gmail.com>
2023-07-16 12:14:40 +0000

    Finish full-fledged-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
-    else
-      flash.now[:danger] = '認証に失敗しました。'
-      render :new
-    end
+  user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    redirect_to user
+  else
+    flash.now[:danger] = '認証に失敗しました。'
+    render :new
+  end
   end
 
   def destroy
